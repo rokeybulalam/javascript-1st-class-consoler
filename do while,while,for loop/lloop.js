525d8ce37810375ae4cf63f19774e46d54a29b33
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-25 18:36:41 +0600

    for, while, do while

---
+++ do while,while,for loop/lloop.js
@@ -0,0 +1,15 @@
+var number = 1;
+
+// for(var i=0; i<10; i++) {
+//     console.log(i);
+// }
+
+// do{
+//     console.log(number);
+//     number++;
+// }while(number<10);
+
+while(number<14){
+    console.log(number);
+    number++;
+}
