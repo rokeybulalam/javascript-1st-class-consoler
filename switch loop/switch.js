ddb6f2d4e5d325270e515619f3be5959478663e8
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-25 17:26:15 +0600

    switch loop

---
+++ switch loop/switch.js
@@ -0,0 +1,15 @@
+var day = 'Saturday';
+
+switch (day) {
+    case 'Friday':
+    console.log("This is our Holiday Day");
+    break;
+    case 'Sunday':
+    console.log("This is our Bazar day");
+    break;
+    case 'Saturday':
+    console.log("This is our learning day");
+    break;
+    default:
+    console.log("Its not good day");
+}
