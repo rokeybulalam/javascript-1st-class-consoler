f74cc0b03f647c5398f3921bb5033482881c5fb8
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-26 09:46:26 +0600

    array

---
+++ array/array.js
@@ -0,0 +1,20 @@
+var aname = [
+    'Rokeybul',
+    'Rabiul',
+    'shohag',
+    30,
+    'musfiq',
+]
+
+// aname.push('mehedi');
+// aname.pop();
+// aname.shift();
+
+// for(i=0; i<aname.length; i++){
+//     console.log(aname[i]);
+// }
+
+// console.log(aname.indexOf('shohag'));
+
+aname.splice(2,3);
+console.log(aname);
