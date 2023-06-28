2ad9b79402c0a048e1898dbdd3b708df788321be
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-28 08:29:51 +0600

    js object

---
+++ object/object.js
@@ -0,0 +1,10 @@
+var info = {
+    MathmaticsName: 'Higher Math',
+    fullName: 'Rokeybul Alam',
+    Age: 23,
+    VillageName: 'Kazlapara',
+    hello: alert('asslamualaium kum')
+}
+
+// console.log(info)
+info.hello
