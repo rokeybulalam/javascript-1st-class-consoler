c9dad2d5ba05da88d2048e588bb5a59c729b4229
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-28 10:22:52 +0600

    function

---
+++ function/function.js
@@ -0,0 +1,12 @@
+function sumMechine(){
+    console.log('My name is Rokeybul Alam');
+}
+
+console.log(sumMechine());
+
+// function sumMechine(a,b){
+//     var sum = a+b;
+//     return sum;
+// }
+
+// console.log(sumMechine(40,39))
